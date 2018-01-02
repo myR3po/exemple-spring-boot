30abcb080f4fa1855b6b1ce9accca2ac167664c0
myR3po <>
2018-01-02 17:48:32 +0100

    TP 08 Templating & WebJar

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO EXEMPLE VALUES ( 1, 'mot', 'de');
+INSERT INTO EXEMPLE VALUES ( 2, 'passe', 'correct');
+INSERT INTO EXEMPLE VALUES ( 3, 'tu', 'peux');
+INSERT INTO EXEMPLE VALUES ( 4, 'te', 'connecter');
